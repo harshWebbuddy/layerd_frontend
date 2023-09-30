537f963733d28c69c96ad1541f1d475b02a5990b
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-30 15:11:01 +0200

    Refactor

--- app/auth/register/components/RegistrationForm.tsx
+++ app/auth/register/components/RegistrationForm.tsx
@@ -11,6 +11,7 @@ export default function RegistrationForm({}: Props) {
   const router = useRouter()
   const submitHandler = (e : any) => {
     e.preventDefault();
+		console.log('submitted!!!!!!!!')
     router.push('/auth/verify/email')
   }
 	return (
