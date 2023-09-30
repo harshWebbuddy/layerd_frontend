3aaec4ff115e996c7da494e973858224bb449268
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-30 17:03:24 +0200

    Page transitions

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,35 @@
 import Layout from "./components/Layout";
-
+("use client");
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return <Layout>{children}</Layout>;
+	const pathname = usePathname();
+
+	return (
+		<AnimatePresence mode="wait">
+			<motion.main
+				key={pathname}
+				initial="initialState"
+				animate="animateState"
+				exit="exitState"
+				transition={{
+					duration: 1,
+				}}
+				variants={{
+					initialState: {
+						opacity: 0,
+					},
+					animateState: {
+						opacity: 1,
+					},
+					exitState: {},
+				}}>
+				{children}
+			</motion.main>
+		</AnimatePresence>
+	);
 }
