ee4c5b18681388de84523ee67f691e722254acb7
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-10-30 10:42:53 +0200

    [refactor] Engine

--- app/dashboard/main/search-engine/page.tsx
+++ app/dashboard/main/search-engine/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function page() {
 	return (
 		<section className="flex flex-col xl:flex-row items-start w-full absolute inset-0 h-full">
-			<div className="w-full mt-20 p-4 min-h-[500px] pl-[58px]">
+			<div className="w-full mt-20 p-4 min-h-[500px] md:pl-[58px]">
 				<PromptInput
 					placeholder="Ask from whopme..."
 					iconSrc="/arrow-right.svg"
@@ -26,10 +26,10 @@ export default function page() {
 					))}
 				</div>
 			</div>
-			<div className="w-full bg-[url('/ai/search-ai.png')] bg-center bg-cover h-full relative">
-				<div className="absolute inset-0 backdrop-blur-xl px-6 h-full">
-					<div className="pt-10 xl:pt-32 h-full flex flex-col max-h-screen">
-						<div className="flex flex-col md:flex-row xl:flex-col 2xl:flex-row gap-y-3 justify-between">
+			<div className="w-full bg-[url('/ai/search-ai.png')] bg-center bg-cover h-full relative min-h-[600px]">
+				<div className="absolute inset-0 backdrop-blur-xl md:px-6 h-full">
+					<div className="pt-6 md:pt-10 xl:pt-32 h-full flex flex-col max-h-screen">
+						<div className="flex flex-col md:flex-row xl:flex-col 2xl:flex-row gap-y-6 justify-between">
 							<div className="flex gap-2">
 								{["Results", "News", "Images", "Videos"].map((tab, index) => (
 									<div
