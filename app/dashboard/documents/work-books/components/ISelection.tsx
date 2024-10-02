ea579239d60e368f3139ec022333438d07e0f3f1
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-10-02 14:49:32 +0200

    [fix] fixed build errors

--- app/dashboard/documents/work-books/components/ISelection.tsx
+++ app/dashboard/documents/work-books/components/ISelection.tsx
@@ -4,41 +4,39 @@ import Selection from "@/app/dashboard/account/me/components/Selection";
 import React from "react";
 
 export default function ISelection() {
-	const selectionItems = [
-		{
-			value: "Workbook 1",
-			label: "Workbook 1",
-		},
-		{
-			value: "Workbook 2",
-			label: "Workbook 2",
-		},
-		{
-			value: "Workbook 3",
-			label: "Workbook 3",
-		},
-		{
-			value: "Workbook 4",
-			label: "Workbook 4",
-		},
-		{
-			value: "Workbook 5",
-			label: "Workbook 5",
-		},
-		{
-			value: "Workbook 6",
-			label: "Workbook 6",
-		},
-	];
-	return (
-		<div className="w-full max-w-6xl">
-			<Selection
-				id="workbook-select"
-				items={selectionItems}
-				label="Select Workbook Name"
-				placeholder="Select"
-				required={false}
-			/>
-		</div>
-	);
+  const selectionItems = [
+    {
+      value: "Workbook 1",
+      label: "Workbook 1",
+    },
+    {
+      value: "Workbook 2",
+      label: "Workbook 2",
+    },
+    {
+      value: "Workbook 3",
+      label: "Workbook 3",
+    },
+    {
+      value: "Workbook 4",
+      label: "Workbook 4",
+    },
+    {
+      value: "Workbook 5",
+      label: "Workbook 5",
+    },
+    {
+      value: "Workbook 6",
+      label: "Workbook 6",
+    },
+  ];
+  return (
+    <div className="w-full max-w-6xl">
+      <Selection
+        items={selectionItems}
+        label="Select Workbook Name"
+        placeholder="Select"
+      />
+    </div>
+  );
 }
