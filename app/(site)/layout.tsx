09d59d57133fc76cbb237f953b9dea1b25d7e067
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-10-18 18:35:44 +0200

    [update] fixed api url error

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,21 +2,12 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import React from "react";
-import { useGoogleOneTapLogin } from "@react-oauth/google";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function layout({ children }: Props) {
-  useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      console.log(credentialResponse);
-    },
-    onError: () => {
-      console.log("Login Failed");
-    },
-  });
   return (
     <main className="bg-[url('/grains.png')] bg-cover overflow-hidden">
       <div className="bg-[#1E1E26]/60 w-full h-full min-h-screen">
